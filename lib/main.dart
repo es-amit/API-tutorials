9375cfa8ea5d12d3173a4e4a75ee8b1acfd5bc47
amit <amit.singh.9312687983@gmail.com>
2023-11-14 21:34:27 +0530

    Loading Network image with API

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:api_tutorials/screens/home_screen.dart';
+import 'package:api_tutorials/screens/tutorial_two.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
+    home: ExampleTwo(),
   ));
 }
