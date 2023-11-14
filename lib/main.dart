ced24836f5a1181b383210aea46e0f836969e84a
amit <amit.singh.9312687983@gmail.com>
2023-11-14 21:58:52 +0530

    Complex JSON structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:api_tutorials/screens/home_screen.dart';
-import 'package:api_tutorials/screens/tutorial_two.dart';
+// import 'package:api_tutorials/screens/home_screen.dart';
+import 'package:api_tutorials/screens/tutorial_three.dart';
+// import 'package:api_tutorials/screens/tutorial_two.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: ExampleTwo(),
+    home: ExampleThree(),
   ));
 }
