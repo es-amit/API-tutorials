5e0f79f6e4d88159a43808e4554805c16a0171b8
amit <amit.singh.9312687983@gmail.com>
2023-11-14 16:03:55 +0530

    Get API call with Null safty

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:api_tutorials/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
