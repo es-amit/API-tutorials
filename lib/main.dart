f6c49c219ebf1819fa19bd170f9c04f5d3aec1d3
amit <amit.singh.9312687983@gmail.com>
2023-11-14 22:11:24 +0530

    working without Models

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 // import 'package:api_tutorials/screens/home_screen.dart';
-import 'package:api_tutorials/screens/tutorial_three.dart';
+import 'package:api_tutorials/screens/tutorial_four.dart';
+//import 'package:api_tutorials/screens/tutorial_three.dart';
 // import 'package:api_tutorials/screens/tutorial_two.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: ExampleThree(),
+    home: ExampleFour(),
   ));
 }
